e78e9945f36abe1307f3a4c76c8d6395516819dd
MJ <80272444+minai621@users.noreply.github.com>
2023-01-24 05:38:44 +0000

    taskInfo

--- api/src/schema/queries.js
+++ api/src/schema/queries.js
@@ -1,43 +1,53 @@
 import {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLNonNull,
-    GraphQLList,
-  } from 'graphql';
-  
-  import NumbersInRange from './types/numbers-in-range';
-  import { numbersInRangeObject } from '../utils';
-  
-  import Task from './types/task';
-  
-  const QueryType = new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      currentTime: {
-        type: GraphQLString,
-        resolve: () => {
-          const isoString = new Date().toISOString();
-          return isoString.slice(11, 19);
-        },
-      },
-      numbersInRange: {
-        type: NumbersInRange,
-        args: {
-          begin: { type: new GraphQLNonNull(GraphQLInt) },
-          end: { type: new GraphQLNonNull(GraphQLInt) },
-        },
-        resolve: function (source, { begin, end }) {
-          return numbersInRangeObject(begin, end);
-        },
-      },
-      taskMainList: {
-        type: new GraphQLList(new GraphQLNonNull(Task)),
-        resolve: async (source, args, { pgApi }) => {
-          return pgApi.taskMainList();
-        },
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLID,
+} from 'graphql';
+
+import NumbersInRange from './types/numbers-in-range';
+import { numbersInRangeObject } from '../utils';
+
+import Task from './types/task';
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    currentTime: {
+      type: GraphQLString,
+      resolve: () => {
+        const isoString = new Date().toISOString();
+        return isoString.slice(11, 19);
       },
     },
-  });
-  
-  export default QueryType;
+    numbersInRange: {
+      type: NumbersInRange,
+      args: {
+        begin: { type: new GraphQLNonNull(GraphQLInt) },
+        end: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: function (source, { begin, end }) {
+        return numbersInRangeObject(begin, end);
+      },
+    },
+    taskMainList: {
+      type: new GraphQLList(new GraphQLNonNull(Task)),
+      resolve: async (source, args, { pgApi }) => {
+        return pgApi.taskMainList();
+      },
+    },
+    taskInfo: {
+      type: Task,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve: async (source, args, { loaders }) => {
+        return loaders.tasks.load(args.id);
+      },
+    },
+  },
+});
+
+export default QueryType;
